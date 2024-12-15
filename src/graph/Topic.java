d1f2d0e6c52d8addc3e9a070de8124f177e4026e
sapirgur <sapir8220@gmail.com>
2024-12-15 10:40:34 +0200

    Topic class built

--- src/graph/Topic.java
+++ src/graph/Topic.java
@@ -4,7 +4,46 @@ import java.util.List;
 
 public class Topic {
     public final String name;
-    private List<Agent> topics;
+    private List<Agent> subscribers;
+    private List<Agent> publishers;
+
+    //CTOR
+    public Topic(String name) {
+        this.name = name;
+        this.subscribers = new ArrayList<>();
+        this.publishers = new ArrayList<>();
+    }
+
+    //Subscribe new agent if not already exist
+    public void subscribe(Agent agent) {
+        if(!subscribers.contains(agent)) {
+            subscribers.add(agent);
+        }
+    }
+
+    // Activate callBack method on publishers
+    public void publish(Message msg) {
+        for (Agent agent : publishers) {
+            agent.callback(name, msg);
+        }
+    }
+
+    //Remove agent from the subscribers
+    public void unsubscribe(Agent agent) {
+        subscribers.remove(agent);
+    }
+
+    //Remove agent from publishers
+    public void removePublisher(Agent agent) {
+        publishers.remove(agent);
+    }
+
+    //Add agent to the publishers
+    public void addPublisher(Agent agent) {
+        if(!publishers.contains(agent)) {
+            publishers.add(agent);
+        }
+    }
 
 
 }
