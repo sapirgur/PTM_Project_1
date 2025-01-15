105c892d1b622afc4729f633d4a6f6461b694e95
sapirgur <sapir8220@gmail.com>
2025-01-15 12:22:47 +0200

    Adding fields to GenericConfig

--- src/configs/GenericConfig.java
+++ src/configs/GenericConfig.java
@@ -1,4 +1,18 @@
 package configs;
 
-public class GenericConfig {
+import graph.Agent;
+import graph.ParallelAgent;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericConfig implements Config {
+
+    private final List<ParallelAgent> agents = new ArrayList<>();
+    private String configFile;
+
+
 }
